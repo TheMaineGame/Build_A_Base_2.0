8d1d3dde1cce84b6c66991bdbe85b97bd71d3de7
Marcus Gosselin <marcus13345@live.com>
2016-03-30 16:22:40 -0400

    game scene almost works

---
+++ Assets/Game/scripts/CallCube.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CallCube : MonoBehaviour 
+{
+	[SerializeField]GameObject m_livingQuarters;
+
+	public void CubeCall () 
+	{
+		m_livingQuarters.SetActive (true);
+	}
+}
