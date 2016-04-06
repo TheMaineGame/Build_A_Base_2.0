a406fc3c0628e9a334d57acdb2dbea338590573a
Brandon Hustus <mutantoctopusdevelopment@outlook.com>
2016-04-06 14:52:08 -0400

    Quick Prevent-Intersect
    Doesn't try to find a new position

--- Assets/Game/scripts/CallCube.cs
+++ Assets/Game/scripts/CallCube.cs
@@ -5,13 +5,29 @@ public class CallCube : MonoBehaviour
 {
     [SerializeField]
     GameObject m_livingQuarters;
-
     [SerializeField]
     Transform cameraPos;
+    [SerializeField]
+    LayerMask buildings;
 
     public void CubeCall()
     {
-        GameObject poop = Instantiate(m_livingQuarters);
-        poop.transform.position = new Vector3(Mathf.Round(cameraPos.position.x), Mathf.Round(cameraPos.position.y), Mathf.Round(cameraPos.position.z));
+        var pos = new Vector3 (
+            Mathf.Round (cameraPos.position.x),
+            Mathf.Round (cameraPos.position.y),
+            Mathf.Round (cameraPos.position.z)
+            );
+        while ()
+        if (!Physics.CheckBox (
+            pos,
+            m_livingQuarters.GetComponent<BoxCollider>().bounds.extents,
+            m_livingQuarters.transform.rotation,
+            buildings)
+            ) {
+            GameObject poop = Instantiate (m_livingQuarters, pos, Quaternion.identity) as GameObject;
+        }
+        else {
+
+        }
     }
 }
