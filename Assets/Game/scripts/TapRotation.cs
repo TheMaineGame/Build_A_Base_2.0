09f6bdce8f1d2c3368a64f178e23855dbde38b4e
Brandon Hustus <mutantoctopusdevelopment@outlook.com>
2016-04-11 16:25:19 -0400

    Sort of okay

---
+++ Assets/Game/scripts/TapRotation.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System;
+
+public class TapRotation : MonoBehaviour, IPointerClickHandler {
+    public void OnPointerClick (PointerEventData eventData) {
+        gameObject.transform.rotation *= Quaternion.Euler (0, 90, 0);
+    }
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
