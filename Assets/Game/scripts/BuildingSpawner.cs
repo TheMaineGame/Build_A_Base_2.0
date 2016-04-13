82bc3f7432857bc7c687114e719ad548b0bb127c
Alex Rediker <arediker@utc4me.org>
2016-04-13 16:06:17 -0400

    BuildingSpawner2.0
    Buliding Spawner is BACK! BEBE!

---
+++ Assets/Game/scripts/BuildingSpawner.cs
@@ -0,0 +1,42 @@
+﻿﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class BuildingSpawner : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler 
+{
+	[SerializeField] GameObject building;
+	private bool pointerDown = false;
+
+	void Start () 
+	{
+
+	}
+
+	void Update () 
+	{
+
+	}
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		Debug.Log ("PointerDown");
+		pointerDown = true;
+	}
+	public void OnPointerUp(PointerEventData eventData)
+	{
+		Debug.Log ("PointerUP");
+		pointerDown = false;
+	}
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		if (pointerDown) {
+			Debug.Log ("PointerExit");
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			if (Physics.Raycast(ray, out hit)) 
+			{
+				eventData.selectedObject = GameObject.Instantiate (building, hit.point, Quaternion.identity) as GameObject;
+			}
+			pointerDown = false;
+		}
+	}
+}
