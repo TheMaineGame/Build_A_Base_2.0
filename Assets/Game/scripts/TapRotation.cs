3004ad0e1835ba48354b2af43329b2e04a207de2
Brandon Hustus <mutantoctopusdevelopment@outlook.com>
2016-04-13 15:20:17 -0400

    No longer rotates after drag

--- Assets/Game/scripts/TapRotation.cs
+++ Assets/Game/scripts/TapRotation.cs
@@ -3,10 +3,21 @@ using UnityEngine.EventSystems;
 using System.Collections;
 using System;
 
-public class TapRotation : MonoBehaviour, IPointerClickHandler {
-    public void OnPointerClick (PointerEventData eventData) {
-        gameObject.transform.rotation *= Quaternion.Euler (0, 90, 0);
+public class TapRotation : MonoBehaviour, IPointerClickHandler, IBeginDragHandler {
+    bool dragged = false;
+
+    public void OnBeginDrag (PointerEventData eventData) {
+        dragged = true;
     }
+
+    public void OnPointerClick (PointerEventData eventData) {
+        if (!dragged) {
+            gameObject.transform.rotation *= Quaternion.Euler (0, 90, 0);
+        }
+        dragged = false;
+    }
+
+    
 
     // Use this for initialization
     void Start () {
