926bd384b10699c19a6736aac2d77c01e1a1f678
theuglysweater <jmarks@mr.preble.com>
2016-04-18 11:47:52 -0400

    WORK SUN

---
+++ Assets/CreditRoll.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditRoll : MonoBehaviour {
+
+    [SerializeField]
+    float speed;
+
+    float pos;
+
+    float start;
+
+    void Start()
+    {
+        start = this.gameObject.transform.localPosition.y;
+    }
+
+    void OnEnable()
+    {
+        pos = start;
+    }
+
+	void Update () {
+        pos += (Time.deltaTime * speed);
+
+        this.transform.localPosition = new Vector3(0,pos,0);
+	}
+}
