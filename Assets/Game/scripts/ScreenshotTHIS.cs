858a90aa7fe5fbc7849678782d788dca1d6ffcc4
Quinten Campbell <qcampbell@utc4me.org>
2016-04-18 13:13:50 -0400

    game over timer

---
+++ Assets/Game/scripts/ScreenshotTHIS.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenshotTHIS : MonoBehaviour {
+
+    [SerializeField]
+    GameObject Canvas;
+    [SerializeField]
+    GameObject input;
+    [SerializeField]
+    GameObject Image;
+
+    AudioSource AS;
+
+    void Start()
+    {
+        AS = GetComponent<AudioSource>();
+    }
+
+    public void OnClick ()
+    {
+        Canvas.SetActive(false);
+        Application.CaptureScreenshot("Screenshot.png");
+        StartCoroutine("Enable");
+	}
+    IEnumerator Enable()
+    {
+        yield return new WaitForSeconds(0.5f);
+        AS.Play();
+        Image.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        Image.SetActive(false);
+        yield return new WaitForSeconds(0.45f);
+        input.SetActive(true);
+        Canvas.SetActive(true);
+
+    }
+}
