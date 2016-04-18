db5e434f216e6baeed0790a0cc181904f5600c25
theuglysweater <jmarks@mr.preble.com>
2016-04-18 11:12:29 -0400

    MENU ANIMATIONS FIXED

---
+++ Assets/Main Menu/UI Buttons/Scripts/MenuAnimations.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuAnimations : MonoBehaviour {
+
+    [SerializeField]
+    Animator Menu;
+    [SerializeField]
+    Animator Camera;
+    [SerializeField]
+    Animator Fade;
+
+    public void StartFade()
+    {
+        Menu.SetTrigger("Trigger");
+    }
+
+    public void MoveCamera()
+    {
+        Camera.SetTrigger("Trigger");
+    }
+
+    public void FadeToRed()
+    {
+        Fade.SetTrigger("Trigger");
+    }
+
+}
