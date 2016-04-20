a8cbbcc5e8cf03200bc78e0cb669c91b23232724
Alex Rediker <redikeralex@utc4me.org>
2016-04-20 09:33:48 -0400

    Gestures
    Gestures AGAIN

---
+++ Assets/Game/scripts/Gestures.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Gestures : MonoBehaviour {
+
+	[SerializeField]
+	Image pointer;
+	[SerializeField]
+	Image bbBoard;
+	public Animator anim;
+	public float gestureTime = 0f;
+
+	void Start ()
+	{
+		pointer.enabled = false;
+		bbBoard.enabled = false;
+	}
+	void FixedUpdate()
+	{
+		gestureTime++;
+	}
+	void Update()
+	{
+		if (gestureTime >= 240f) 
+		{
+			pointer.enabled = true;
+			bbBoard.enabled = true;
+		}
+		if (Input.GetButtonDown ("Fire1")) 
+		{
+			gestureTime = 0f;
+			pointer.enabled = false;
+			bbBoard.enabled = false;
+		}
+	}
+}
