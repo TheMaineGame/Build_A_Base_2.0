eeebe5e269b0a5aaf16be1995d7ef480261c9d46
Alex Rediker <redikeralex@utc4me.org>
2016-04-20 10:34:08 -0400

    Gesture Fixed Again
    ahh poo

--- Assets/Game/scripts/Gestures.cs
+++ Assets/Game/scripts/Gestures.cs
@@ -8,35 +8,10 @@ public class Gestures : MonoBehaviour {
 	Image pointer;
 	[SerializeField]
 	Image bbBoard;
-	[SerializeField]
-	GameObject gameOver;
 
-	public Animator anim;
-	public float gestureTime = 0f;
-
-	void Start ()
-	{
-		pointer.enabled = false;
-		bbBoard.enabled = false;
-	}
-	void FixedUpdate()
-	{
-		gestureTime++;
-	}
 	void Update()
 	{
-		if (gestureTime >= 240f) 
-		{
-			pointer.enabled = true;
-			bbBoard.enabled = true;
-		}
 		if (Input.GetButtonDown ("Fire1")) 
-		{
-			gestureTime = 0f;
-			pointer.enabled = false;
-			bbBoard.enabled = false;
-		}
-		if (gameOver.activeSelf) 
 		{
 			pointer.enabled = false;
 			bbBoard.enabled = false;
