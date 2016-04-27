419aeff56400e72648cdebb1fc8d9ab8a34a8740
Brandon Hustus <mutantoctopusdevelopment@outlook.com>
2016-04-27 11:43:15 -0400

    Wu

---
+++ Assets/Game/scripts/Trash.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class Trash : MonoBehaviour, IDropHandler {
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
