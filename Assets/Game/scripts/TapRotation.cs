8bc98e56717697967172ce991a229f897b3f2767
Brandon Hustus <mutantoctopusdevelopment@outlook.com>
2016-04-27 15:36:52 -0400

    Woahg

--- Assets/Game/scripts/TapRotation.cs
+++ Assets/Game/scripts/TapRotation.cs
@@ -6,10 +6,14 @@ using System;
 
 public class TapRotation : MonoBehaviour, IPointerClickHandler, IBeginDragHandler {
     bool dragged = false;
+    bool rotating = false;
     BoxCollider box;
 
     [SerializeField]
     LayerMask buildingLayer;
+
+    [SerializeField]
+    LayerMask ghostLayer;
 
     [SerializeField]
     float riseUnitsPerSec = 1;
@@ -32,7 +36,7 @@ public class TapRotation : MonoBehaviour, IPointerClickHandler, IBeginDragHandle
     }
 
     public void OnPointerClick (PointerEventData eventData) {
-        if (!dragged) {
+        if (!dragged && !rotating) {
             Rotate ();
         }
         dragged = false;
@@ -68,16 +72,31 @@ public class TapRotation : MonoBehaviour, IPointerClickHandler, IBeginDragHandle
     }
 
     IEnumerator RotateCoroutine (Quaternion newRot) {
+        rotating = true;
+        // Variable setup
         var time = 0f;
         var oPos = gameObject.transform.position; // for "orginal position"
         var newPos = gameObject.transform.position + Vector3.up * levitateHeight;
         var riseTime = levitateHeight / riseUnitsPerSec;
+        var ghost = new GameObject (gameObject.name + " ghost", typeof (BoxCollider));
+        { // Scope for creating the ghost, so unnecessary variables get GC'd
+            var boundsCenter = box.bounds.center;
+            ghost.transform.position = oPos;
+            if (!Mathf.IsPowerOfTwo (ghostLayer.value))
+                throw new ArgumentException ("Ghost layer mask is not a single layer");
+            ghost.layer = (int) Mathf.Log(ghostLayer.value,2);
+            var ghostbox = ghost.GetComponent<BoxCollider> ();
+            ghostbox.center = (newRot * (boundsCenter - oPos)) + oPos;
+            ghostbox.size = newRot * box.size;
+        }
+        // Rising
         while (time < riseTime) {
             time += Time.deltaTime;
             gameObject.transform.position = Vector3.Lerp (oPos, newPos, time / riseTime);
             yield return null;
         }
         time = 0;
+        // Rotating
         var oRot = gameObject.transform.rotation;
         while (time < rotationDuration) {
             time += Time.deltaTime;
@@ -85,12 +104,15 @@ public class TapRotation : MonoBehaviour, IPointerClickHandler, IBeginDragHandle
             yield return null;
         }
         time = 0;
+        // Lowering
         var lowTime = levitateHeight / lowerUnitsPerSec;
         while (time < lowTime) {
             time += Time.deltaTime;
             gameObject.transform.position = Vector3.Lerp (newPos, oPos, time / lowTime);
             yield return null;
         }
+        Destroy (ghost);
+        rotating = false;
     }
 
     void Start () {
