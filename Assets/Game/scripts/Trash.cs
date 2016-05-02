9c3ee166194b48e3ed19017781a612a2b8cbdd12
Brandon Hustus <mutantoctopusdevelopment@outlook.com>
2016-05-02 16:27:40 -0400

    I'M THE TRASH MAN
    I EAT GARBAGE

--- Assets/Game/scripts/Trash.cs
+++ Assets/Game/scripts/Trash.cs
@@ -1,8 +1,12 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
 using System.Collections;
+using System;
 
 public class Trash : MonoBehaviour, IDropHandler {
+    public void OnDrop (PointerEventData eventData) {
+        Destroy (eventData.pointerDrag);
+    }
 
     // Use this for initialization
     void Start () {
