2ff5fa67e4a7d1a8d4ad7829f73edbe5b13b0115
Brandon Hustus <mutantoctopusdevelopment@outlook.com>
2016-05-09 14:48:23 -0400

    Hey count stuff

--- Assets/Game/scripts/Trash.cs
+++ Assets/Game/scripts/Trash.cs
@@ -6,15 +6,6 @@ using System;
 public class Trash : MonoBehaviour, IDropHandler {
     public void OnDrop (PointerEventData eventData) {
         Destroy (eventData.pointerDrag);
-    }
-
-    // Use this for initialization
-    void Start () {
-
-    }
-
-    // Update is called once per frame
-    void Update () {
-
+        CallCube.count--;
     }
 }
